5c2b7d0c0d8dd83a8176761eae4d1ae71a89bd5d
muso-zakh <ccvaf0803@gmail.com>
2025-06-04 18:26:05 +0500

    7_9

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')  # os.getenv emas
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
